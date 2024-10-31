05ef605be3ff5146bc5e800091524a236ba62e7f
minseok <pms950106@gmail.com>
2024-10-31 17:17:53 +0900

    추상화된 프로토콜 사용 단계 추가

--- CreateCalculator.playground/Contents.swift
+++ CreateCalculator.playground/Contents.swift
@@ -37,11 +37,12 @@ print("\(first) / \(second) = \(calculator.div(first, second))")
 print("\(first) % \(second) = \(calculator.rem(first, second))")
 */
 
+/*
 // Lv3
 // 상속을 위한 부모 클래스
 class Operation {
-    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
-        return 0 // Opertaion이 따로 쓰이지 않고, 오버라이딩 할 부분이므로 기본 반환 값으로 0을 설정해두었습니다
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double { // 나눗셈이나 나머지 연산에서 소수점이 나오는 것을 대비하고 float 대비 더 정확한 값이 나왔으면 해서 double로 선언하였습니다
+        return 0 // Opertaion이 따로 쓰이지 않고, 오버라이딩 할 부분이므로 기본 반환 값을 0으로 설정해두었습니다
     }
 }
 
@@ -107,3 +108,64 @@ print("\(first) + \(second) = \(calculator.subOperation.calculate(first, second)
 print("\(first) * \(second) = \(calculator.mulOperation.calculate(first, second))")
 print("\(first) / \(second) = \(calculator.divOperation.calculate(first, second))")
 print("\(first) % \(second) = \(calculator.remOperation.calculate(first, second))")
+*/
+
+//Lv4 추상화 된 AbsrtractOperation 프로토콜 만들기
+protocol AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double
+}
+
+class AddOperation: AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber + secondNumber
+    }
+}
+
+class SubOperation: AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber - secondNumber
+    }
+}
+
+class MulOperataion: AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber * secondNumber
+    }
+}
+
+class DivOperation: AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        // 0으로 나눌 때의 예외 처리 필요
+        return firstNumber / secondNumber
+    }
+}
+
+class RemOperation: AbstractOperation {
+    func calculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return firstNumber.truncatingRemainder(dividingBy: secondNumber)
+    }
+}
+
+class Calculator {
+    var operation: AbstractOperation
+    
+    init(parameterOperatin: AbstractOperation) {
+        self.operation = parameterOperatin
+    }
+    
+    func performCalculate(_ firstNumber: Double, _ secondNumber: Double) -> Double {
+        return operation.calculate(firstNumber, secondNumber)
+    }
+}
+
+// 출력 구현부
+let firstNumber: Double = 12
+let secondNumber: Double = 5
+
+let definedOperation: [AbstractOperation] = [AddOperation(), SubOperation(), MulOperataion(), DivOperation(), RemOperation()]
+
+for currentOperation in definedOperation {
+    let calculate = Calculator(parameterOperatin: currentOperation)
+    print(calculate.performCalculate(firstNumber, secondNumber))
+}
+가
